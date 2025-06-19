164d92a2d96d3e5061cbb0e4cac4e9abe11bc8c4
Haley <98135680+haleyher@users.noreply.github.com>
2025-06-20 00:39:23 +0300

    hugeeeee updates

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,13 +9,16 @@ var active = {
 
 const Header = () => (
     <div className="center">
+        <br></br>
+        <br></br>
         <img src="./logo.png" alt="nice" className="header-img"></img>
+        <br></br>
         <ul className='nav-center'>
             <li><NavLink exact to='/' activeStyle={active} >About Us</NavLink></li>
-            <li><NavLink to='/exec' activeStyle={active} >Executive Board</NavLink></li>
-            <li><NavLink to='/events' activeStyle={active} >Upcoming Events</NavLink></li>
-            <li><NavLink to='/comserve' activeStyle={active} >Community Service</NavLink></li>
-            <li><NavLink to='/contact' activeStyle={active} >Contact Us</NavLink></li>
+            <li><NavLink to='/exec' activeStyle={active} >E-Board</NavLink></li>
+            <li><NavLink to='/events' activeStyle={active} >Events</NavLink></li>
+            <li><NavLink to='/comserve' activeStyle={active} >CommServe</NavLink></li>
+            <li><NavLink to='/contact' activeStyle={active} >Contact</NavLink></li>
             <li><NavLink to='/sponsors' activeStyle={active} >Sponsors</NavLink></li>
             <a href="https://forms.gle/1wW6Ag3m8BWxw4Ed8" target="_blank" rel="noopener noreferrer" ><button className='button'>Sign Up!</button></a>
         </ul>
--- src/index.js
+++ src/index.js
@@ -8,4 +8,5 @@ render((
     <HashRouter>
         <App />
     </HashRouter>
+    
 ), document.getElementById('root'));
