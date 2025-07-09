35173c59750667846a21bc09b3e0531692026e92
Haley <98135680+haleyher@users.noreply.github.com>
2025-07-09 11:30:16 +0300

    committ

--- src/components/AlertBar.jsx
+++ src/components/AlertBar.jsx
@@ -4,7 +4,7 @@ import '../styles.css';
 const AlertBar = () => {
   return (
     <div className="alert-bar">
-      🚨 Collab Competition with Algo Ed — <a href="https://www.algoed.co/competitions/mit-shpe-science-engineering-competition" target="_blank" rel="noopener noreferrer">Click here to learn more</a>!
+      🚨 Upcoming Event with Algo Ed: Hosting a STEM competition for middle and high school students — <a href="https://www.algoed.co/competitions/mit-shpe-science-engineering-competition" target="_blank" rel="noopener noreferrer">Click here to learn more</a>!
     </div>
   );
 };
